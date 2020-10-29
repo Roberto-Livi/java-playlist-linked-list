3f6733e51e55bce83084611b6ccc326ff9124eae
Roberto-Livi <robertomlivi@outlook.com>
2020-10-28 21:46:50 -0400

    playing the current song in the LinkedList with listIterator.next().toString()

--- playlist/src/com/company/Main.java
+++ playlist/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
 
@@ -10,17 +11,22 @@ public class Main {
     public static void main(String[] args) {
         Album album = new Album("Stormbringer", "Deep Purple");
         album.addSong("Stormbringer", 4.6);
-        album.addSong("Love don't mean a thing", 4.5);
-        album.addSong("Holy man", 4.22);
-        album.addSong("Hold on", 4.3);
-        album.addSong("Lady double dealer", 5.6);
-        album.addSong("You can't do it right", 3.21);
-        album.addSong("High ball shooter", 6.23);
-        album.addSong("The gypsy", 4.27);
-        album.addSong("Soldier of fortune", 4.2);
+        album.addSong("Love don't mean a thing", 4.22);
+        album.addSong("Holy man", 4.3);
+        album.addSong("Hold on", 5.6);
+        album.addSong("Lady double dealer", 3.21);
+        album.addSong("You can't do it right", 6.23);
+        album.addSong("High ball shooter", 4.27);
+        album.addSong("The gypsy", 4.2);
+        album.addSong("Soldier of fortune", 3.13);
+        albums.add(album);
 
         album = new Album("For those about to rock", "AC/DC");
         album.addSong("For those about to rock", 5.44);
+        album.addSong("I put the finger on you", 3.25);
+        album.addSong("Let's go", 3.45);
+        album.addSong("Inject the venom", 3.33);
+        album.addSong("Snowballed", 4.51);
         albums.add(album);
 
         LinkedList<Song> playList = new LinkedList<Song>();
@@ -36,7 +42,16 @@ public class Main {
         play(playList);
 
 
-
-
     }
+
+    private static void play(LinkedList<Song> playList) {
+        ListIterator<Song> listIterator = playList.listIterator();
+        if(playList.size() == 0){
+            System.out.println("No songs in playlist");
+            return;
+        } else {
+            System.out.println("Now playing " + listIterator.next().toString());
+        }
+    }
+
 }
