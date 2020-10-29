d1366ade2afa9d3dca3ba28db4f810849218adea
Roberto-Livi <robertomlivi@outlook.com>
2020-10-28 21:27:49 -0400

    testing the methods implemented inside the Main method

--- playlist/src/com/company/Main.java
+++ playlist/src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
+    private static ArrayList<Album> albums = new ArrayList<Album>();
+
     public static void main(String[] args) {
+        Album album = new Album("Stormbringer", "Deep Purple");
+        album.addSong("Stormbringer", 4.6);
+        album.addSong("Love don't mean a thing", 4.5);
+        album.addSong("Holy man", 4.22);
+        album.addSong("Hold on", 4.3);
+        album.addSong("Lady double dealer", 5.6);
+        album.addSong("You can't do it right", 3.21);
+        album.addSong("High ball shooter", 6.23);
+        album.addSong("The gypsy", 4.27);
+        album.addSong("Soldier of fortune", 4.2);
+
+        album = new Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        albums.add(album);
+
+        LinkedList<Song> playList = new LinkedList<Song>();
+        albums.get(0).addToPlayList("You can't do it right", playList);
+        albums.get(0).addToPlayList("Holy man", playList);
+        albums.get(0).addToPlayList("Speed king", playList);
+        albums.get(0).addToPlayList(9, playList);
+        albums.get(1).addToPlayList(8, playList);
+        albums.get(1).addToPlayList(3, playList);
+        albums.get(1).addToPlayList(2, playList);
+        albums.get(1).addToPlayList(24, playList);
+
+        play(playList);
+
+
+
 
     }
 }
